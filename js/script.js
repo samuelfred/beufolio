c817d9bba0356399c6a7edab557f94f06696a704
Samuel <48195271+samuelfred@users.noreply.github.com>
2019-07-11 18:32:03 +1200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 console.log('hi');
 document.getElementById("mynav").style.width = "0%";
+
 $(document).ready(function(){
 
 	 $('.fa').click(function(){
-	 	document.getElementById("mynav").style.width = "100%";
+	 	$('#mynav').toggle();
+	 	// document.getElementById("mynav").style.width = "100%";
 	 })
 	 $('.close').click(function(){
-	 	document.getElementById("mynav").style.width = "0%";
+	 	$('#mynav').hide();
+	 	// document.getElementById("mynav").style.width = "0%";
 	 })
 })
